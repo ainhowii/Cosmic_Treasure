6ff6d6eb416468599ccd5027744139554aef0754
pierre <merzonpm@gmail.com>
2024-04-25 20:03:47 +0200

    player

--- CosmicTreasure/Assets/CosmicTreasure_Root/Scripts/PlayerController.cs
+++ CosmicTreasure/Assets/CosmicTreasure_Root/Scripts/PlayerController.cs
@@ -6,45 +6,110 @@ using UnityEngine.Scripting.APIUpdating;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private Rigidbody _rb;
-    [SerializeField] private float _speed = 5;
-    [SerializeField] private float _turnSpeed = 360;
-    private Vector3 _input;
+    public Rigidbody2D body;
 
-    private void Update()
+    public SpriteRenderer spriteRenderer;
+
+    public List<Sprite> nSprites;
+    public List<Sprite> neSprites;
+    public List<Sprite> eSprites;
+    public List<Sprite> seSprites;
+    public List<Sprite> sSprites;
+
+    public float walkSpeed;
+
+    public float frameRate;
+
+    float idleTime;
+
+    Vector2 direction;
+
+    private void Start()
     {
-        GatherInput();
-        Look();
-    }
-
-    private void FixedUpdate()
-    {
-        Move();
-    }
-
-    void GatherInput()
-    {
-        _input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-    }
-
-    void Look()
-    {
-        if (_input != Vector3.zero)
-        {
-            var matrix = Matrix4x4.Rotate(Quaternion.Euler(0, 45, 0));
-
-            var skewedInput = matrix.MultiplyPoint3x4(_input);
-
-            var relative = (transform.position + _input) - transform.position;
-            var rot = Quaternion.LookRotation(relative, Vector3.up);
-
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, _turnSpeed * Time.deltaTime);
-        }
         
     }
 
-    void Move()
+    private void Update()
     {
-        _rb.MovePosition(transform.position + (transform.forward * _input.magnitude) * _speed * Time.deltaTime);
+        direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
+
+        body.velocity = direction * walkSpeed;
+
+        HandleSpriteFlip();
+        SetSprite();
     }
+
+    void SetSprite()
+    {
+        List<Sprite> directionSprites = GetSpriteDirection();
+
+        if (directionSprites != null)
+        { //holding a direction
+
+            float playTime = Time.time - idleTime;
+            int totalFrames = (int)(playTime * frameRate);
+            int frame = totalFrames % directionSprites.Count;
+
+            spriteRenderer.sprite = directionSprites[frame];
+        }
+        else
+        {
+            idleTime = Time.time;
+        }
+
+    }
+
+    void HandleSpriteFlip()
+    {
+        if (!spriteRenderer.flipX && direction.x < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (spriteRenderer.flipX && direction.x > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+
+    }
+
+    List<Sprite> GetSpriteDirection()
+    {
+        List<Sprite> selectedSprites = null;
+
+        if (direction.y > 0) //North
+        {
+            if (Mathf.Abs(direction.x) > 0) //east or west
+            {
+                selectedSprites = neSprites;
+            }
+            else //neutral X
+            {
+                selectedSprites = nSprites;
+            }
+        }
+        else if (direction.y < 0) //South
+        {
+            if (Mathf.Abs(direction.x) > 0) //east or west
+            {
+                selectedSprites = seSprites;
+            }
+            else
+            {
+                selectedSprites = sSprites;
+            }
+        }
+        else //neutral
+        {
+            if (Mathf.Abs(direction.x) > 0) //east or west
+            {
+                selectedSprites = eSprites;
+            }
+        }
+
+        return selectedSprites;
+
+    }
+
+   
+
 }
