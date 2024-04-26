848e97aaf2a82e60914c03f6644e67dfff92198a
pierre <merzonpm@gmail.com>
2024-04-26 19:31:56 +0200

    enemy and bullets

--- CosmicTreasure/Assets/CosmicTreasure_Root/Scripts/EnemyFollow.cs
+++ CosmicTreasure/Assets/CosmicTreasure_Root/Scripts/EnemyFollow.cs
@@ -7,14 +7,19 @@ public class EnemyFollow : MonoBehaviour
     public float speed;
     public float lineOfSite;  //TE PERSIGUE MIENTRAS ESTÉS EN ESTE RANGO
     public float shootingRange;  //EN ESTE RANGO TE DEJA DE PERSEGUIR Y TE DISPARA
-    public float fireRate = 1f;
+    public float fireRate = 1f;  //PARA QUE NO TE TIRE UNA RAFAGA DE TIROS
+    private bool playerPositionSaved;  //Guardar posición del jugador
+    private Vector2 playerPosOnShoot;
+    private float nextFireTime;
     public GameObject bullet;
-    public GameObject bulletParent;
+    [SerializeField] GameObject bulletParent;
     private Transform player;
 
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        playerPositionSaved = false;
+
 
     }
 
@@ -25,11 +30,26 @@ public class EnemyFollow : MonoBehaviour
         {
             transform.position = Vector2.MoveTowards(this.transform.position, player.position, speed * Time.deltaTime);
         }
-        else if (distanceFromPlayer <= shootingRange)
+        else if (distanceFromPlayer <= shootingRange && nextFireTime <Time.time)
         {
-            Instantiate(bullet, bulletParent.transform.position, Quaternion.identity);
+            if (playerPositionSaved == false)
+            {
+                Debug.Log("Entra a if");
+                playerPosOnShoot = player.position;
+                Debug.Log("PlayerPosOnShoot" + playerPosOnShoot);
+                playerPositionSaved = true;
+
+                Instantiate(bullet, bulletParent.transform.position, Quaternion.identity);
+
+                Invoke("BoolReset", 1);
+            }
         }
        
+    }
+
+    private void BoolReset()
+    {
+        playerPositionSaved = false;
     }
 
     private void OnDrawGizmosSelected()
