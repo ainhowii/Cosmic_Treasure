8a853ccbf368768db1c0c542fc3f02e2818d6678
mote44 <137623605+mote44@users.noreply.github.com>
2024-04-29 23:08:55 +0200

    ProductoPuntoOkBase

--- CosmicTreasure/Assets/CosmicTreasure_Root/Scripts/Escenario/ProductoPunto.cs
+++ CosmicTreasure/Assets/CosmicTreasure_Root/Scripts/Escenario/ProductoPunto.cs
@@ -6,13 +6,13 @@ public class ProductoPunto : MonoBehaviour
 {
 
     [SerializeField] Transform player;  //posicion player
-    [SerializeField] Vector2 objectVector;
+    [SerializeField] Vector2 objectVector; //Define la linea divisoria
     [SerializeField] Vector2 pointToPlayer; //Vector entre el punto y el player
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
-        objectVector = Vector2.up;
+        objectVector = new Vector2(2,4).normalized;
     }
 
     // Update is called once per frame
@@ -22,13 +22,21 @@ public class ProductoPunto : MonoBehaviour
         float dotProduct =Vector2.Dot(objectVector, pointToPlayer);
         Debug.Log(dotProduct);
 
-        if (dotProduct > 0) { gameObject.GetComponentInParent<SpriteRenderer>().sortingLayerName = "New Layer 1"; }
-        else { gameObject.GetComponentInParent<SpriteRenderer>().sortingLayerName = "New Layer 2"; }
+        if (dotProduct > 0)
+        {
+            gameObject.GetComponentInParent<SpriteRenderer>().sortingLayerName = "New Layer 1";
+            gameObject.GetComponentInParent<SpriteRenderer>().color = new Color(1, 1, 1, .5f);
+        }
+        else
+        {
+            gameObject.GetComponentInParent<SpriteRenderer>().sortingLayerName = "New Layer 2";
+            gameObject.GetComponentInParent<SpriteRenderer>().color = new Color(1, 1, 1, 1);
+        }
     }
 
-    private void OnDrawGizmos()
+    /*private void OnDrawGizmos()
     {
         Debug.DrawLine(transform.position, player.position);
         
-    }
+    }*/
 }
