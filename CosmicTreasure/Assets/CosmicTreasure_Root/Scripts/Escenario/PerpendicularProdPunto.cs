8b884cf8d45114c7aec8ed9f76d41a0654bbeae3
mote44 <137623605+mote44@users.noreply.github.com>
2024-05-02 10:57:03 +0200

    ajuste script puertas

--- CosmicTreasure/Assets/CosmicTreasure_Root/Scripts/Escenario/PerpendicularProdPunto.cs
+++ CosmicTreasure/Assets/CosmicTreasure_Root/Scripts/Escenario/PerpendicularProdPunto.cs
@@ -2,9 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ProductoPunto : MonoBehaviour
+public class PerpendicularProdPunto : MonoBehaviour
 {
-
     [SerializeField] Transform player;  //posicion player
     [SerializeField] Vector2 objectVector1; //Define la linea divisoria NE-SO
     [SerializeField] Vector2 objectVector2; //Define la linea divisoria NO-SE
@@ -21,49 +20,43 @@ public class ProductoPunto : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        pointToPlayer = (player.position-transform.position).normalized;
+        pointToPlayer = (player.position - transform.position).normalized;
         float dotProduct1 = Vector2.Dot(objectVector1, pointToPlayer);  //ProductoPunto que divide NE-SO
         float dotProduct2 = Vector2.Dot(objectVector2, pointToPlayer);  //ProductoPunto que divide NO-SE
         playerDistance = Vector2.Distance(transform.position, player.transform.position);
         Debug.Log("Distance = " + playerDistance);
 
-       // Debug.Log("pp1 = " + dotProduct1);
-       // Debug.Log("pp2 = " + dotProduct2);
+        // Debug.Log("pp1 = " + dotProduct1);
+        // Debug.Log("pp2 = " + dotProduct2);
 
-        if (dotProduct1 > 0 && dotProduct2 > 0 ) //Si player está detrás
+        if (dotProduct1 < 0 && dotProduct2 < 0) //Si player está detrás
         {
             gameObject.GetComponentInParent<SpriteRenderer>().sortingLayerName = "New Layer 1";
             gameObject.GetComponentInParent<SpriteRenderer>().color = new Color(1, 1, 1, .5f);
             Debug.Log("Detrás");
-            if(playerDistance > 6) { gameObject.GetComponentInParent<SpriteRenderer>().color = new Color(1, 1, 1, 1); }
+            if (playerDistance > 6) { gameObject.GetComponentInParent<SpriteRenderer>().color = new Color(1, 1, 1, 1); }
         }
-        else if(dotProduct1 <0 && dotProduct2 > 0) //Si player está delante
+        else if (dotProduct1 < 0 && dotProduct2 > 0) //Si player está delante
         {
             gameObject.GetComponentInParent<SpriteRenderer>().sortingLayerName = "New Layer 2";
             gameObject.GetComponentInParent<SpriteRenderer>().color = new Color(1, 1, 1, 1);
             Debug.Log("Delante");
         }
-        else if(dotProduct1 <0 && dotProduct2 < 0) //Si player delante a la izq
+        else if (dotProduct1 > 0 && dotProduct2 > 0) //Si player delante a la der
         {
             gameObject.GetComponentInParent<SpriteRenderer>().sortingLayerName = "New Layer 2";
             gameObject.GetComponentInParent<SpriteRenderer>().color = new Color(1, 1, 1, 1);
             Debug.Log("DelanteIzq");
         }
-        else if(dotProduct1 >0 && (dotProduct2< 0)) //Si player detras a la izq
+        else if (dotProduct1 > 0 && (dotProduct2 < 0)) //Si player detras a la der
         {
             gameObject.GetComponentInParent<SpriteRenderer>().sortingLayerName = "New Layer 1";
             gameObject.GetComponentInParent<SpriteRenderer>().color = new Color(1, 1, 1, 1f);
             Debug.Log("DetrásIzq");
-            if (transform.position.x < player.transform.position.x && playerDistance < 6)       //Prueba: Usar la distancia para entrar a los IF
+            if (transform.position.x > player.transform.position.x && playerDistance < 6)       //Prueba: Usar la distancia para entrar a los IF
             {
                 gameObject.GetComponentInParent<SpriteRenderer>().color = new Color(1, 1, 1, .5f);
             }
         }
     }
-
-    /*private void OnDrawGizmos()
-    {
-        Debug.DrawLine(transform.position, player.position);
-        
-    }*/
 }
