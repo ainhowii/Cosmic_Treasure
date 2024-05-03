a7cf8837936653c551fc83d972380d92763ac076
PierreMerzon <pimeba.alumno@fundacionpjo.es>
2024-05-03 13:43:47 +0200

    vkjdsks

--- CosmicTreasure/Assets/CosmicTreasure_Root/Scripts/EnemyTest.cs
+++ CosmicTreasure/Assets/CosmicTreasure_Root/Scripts/EnemyTest.cs
@@ -29,6 +29,10 @@ public class EnemyTest : MonoBehaviour
     private int randomNumber;
     private SpriteRenderer spriteRenderer;
 
+    [Header("Random Patrol")]
+    public float radius;
+    public Transform centrePoint;
+
     [Header("States Enemy")]
     private bool isFollowing;
     private bool isShooting;
@@ -92,6 +96,33 @@ public class EnemyTest : MonoBehaviour
                 transform.position = Vector2.MoveTowards(transform.position, movementPoints[randomNumber].position, speedMovement * Time.deltaTime);
                 Debug.Log("We dont seen");
             }
+        }
+        if (isFollowing == false)
+        {
+            isFollowing = false;
+            Searching();
+        }
+    }
+
+    private void Searching()  //ESTADO DE BUSQUEDA
+    {
+        isFollowing = false;
+        bool RandomPoint(Vector3 center, float range, out Vector3 result)
+        {
+
+            Vector3 randomPoint = center + Random.insideUnitSphere * range; //random point in a sphere 
+            NavMeshHit hit;
+
+            if (NavMesh.SamplePosition(randomPoint, out hit, 1.0f, NavMesh.AllAreas))
+            {
+                //the 1.0f is the max distance from the random point to a point on the navmesh, might want to increase if range is big
+                //or add a for loop like in the documentation
+                result = hit.position;
+                return true;
+            }
+
+            result = Vector3.zero;
+            return false;
         }
     }
 
