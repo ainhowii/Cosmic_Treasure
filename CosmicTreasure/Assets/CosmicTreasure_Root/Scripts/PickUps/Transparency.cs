dfb5a39f221cc4c69c67be42c739c3a26ed48aa6
PierreMerzon <153851795+PierreMerzon@users.noreply.github.com>
2024-05-06 18:36:29 +0200

    gfhd

---
+++ CosmicTreasure/Assets/CosmicTreasure_Root/Scripts/PickUps/Transparency.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transparency : MonoBehaviour
+{
+    SpriteRenderer ren;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Alpha1)) //&& equippedTrans)
+        {
+            TransAction();
+        }
+    }
+
+    private void TransAction()  //Cambia la transparencia del player
+    {
+        ren = GetComponent<SpriteRenderer>();
+        ren.color = new Color(1f, 1f, 1f, 0.45f);
+        Invoke("Return", 10);
+    }
+
+    private void Return()  //Vuelve a ser opaco el player
+    {
+        ren.color = new Color(1f, 1f, 1f, 1f);
+    }
+}
