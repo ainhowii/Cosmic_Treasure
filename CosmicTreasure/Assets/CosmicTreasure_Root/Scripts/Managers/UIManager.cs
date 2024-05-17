5467e2cfa223b8f3168a2a2533a2afbea15c3f4e
mote44 <137623605+mote44@users.noreply.github.com>
2024-05-17 16:18:09 +0200

    ui

--- CosmicTreasure/Assets/CosmicTreasure_Root/Scripts/Managers/UIManager.cs
+++ CosmicTreasure/Assets/CosmicTreasure_Root/Scripts/Managers/UIManager.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
+
 
 public class UIManager : MonoBehaviour
 {
